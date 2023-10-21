e0669c163b0b71e1695b742dcd8b66db7fe27d52
= <=>
2023-10-21 12:03:13 -0700

    Display massage to get input from user

--- Hack3.c
+++ Hack3.c
@@ -12,7 +12,13 @@ int main()
 
     double totalGB, total_used, averageDailyUse,Max_Use;
     int currentDay, Remaning_Days;
- 
- 
+    
+    printf("Number of GB in the plan per 30 day period \n");
+    scanf("%lf", &totalGB);
+    printf("The current day in the 30 day period (in the range 1, first day, 30 for the last day)\n");
+    scanf("%d", &currentDay);
+    printf("The total number of GB used so far\n");
+    scanf("%lf", &total_used);
+
     return 0;
 }
