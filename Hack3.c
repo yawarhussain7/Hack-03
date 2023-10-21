8e344001140df4b649274a4978f5a5c13eeb789b
= <=>
2023-10-21 12:00:28 -0700

    Adding author Name Program introduction and basic C syntax

---
+++ Hack3.c
@@ -0,0 +1,17 @@
+/*
+Author: Yawar Hussain
+                ************** Program introduction ***********************
+This program calculate the user data package if there data is almost finished , data is completely finished or near to finished 
+it inform the user 
+*/
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+
+    double totalGB, total_used, averageDailyUse,Max_Use;
+    int currentDay, Remaning_Days;
+ 
+    return 0;
+}
