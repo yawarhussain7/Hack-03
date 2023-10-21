394efc19d07d9bf47a0df3d8f64fa3582fb57f6e
= <=>
2023-10-21 12:44:02 -0700

    else statement execute and show massage the user that his/her Package is expired

--- Hack3.c
+++ Hack3.c
@@ -1,8 +1,8 @@
 /*
 Author: Yawar Hussain
                 ************** Program introduction ***********************
-This program calculate the user data package if there data is almost finished , data is completely finished or near to finished 
-it inform the user 
+This program calculate the user data package if there data is almost finished , data is completely finished or near to finished
+it inform the user
 */
 #include <stdio.h>
 #include <stdlib.h>
@@ -10,9 +10,9 @@ it inform the user
 int main()
 {
 
-    double totalGB, total_used, averageDailyUse,Max_Use,Remaning_GB;
+    double totalGB, total_used, averageDailyUse, Max_Use, Remaning_GB;
     int currentDay, Remaning_Days;
-    
+
     printf("Number of GB in the plan per 30 day period : ");
     scanf("%lf", &totalGB);
     printf("The current day in the 30 day period (in the range 1, first day, 30 for the last day) : ");
@@ -23,24 +23,29 @@ int main()
     Remaning_Days = 30 - currentDay;
     Remaning_GB = totalGB - total_used;
     averageDailyUse = total_used / currentDay;
-    Max_Use = totalGB / 30 ;
+    Max_Use = totalGB / 30;
 
     printf("%d days used, %d days remaining\n", currentDay, Remaning_Days);
     printf("Average daily use: %.3lf GB/day\n", averageDailyUse);
 
-    if(averageDailyUse > Max_Use)
+    if (averageDailyUse > Max_Use)
     {
-         printf("You are EXCEEDING your average daily use (%.3lf GB/day).\n", Max_Use);
+        printf("You are EXCEEDING your average daily use (%.3lf GB/day).\n", Max_Use);
         double suggestion = (averageDailyUse - Max_Use) * 30;
-        printf("Continuing this high usage, you'll exceed your data plan by %.1lf GB.\n",suggestion);
+        printf("Continuing this high usage, you'll exceed your data plan by %.1lf GB.\n", suggestion);
     }
-    
+
     else if (averageDailyUse < Max_Use)
     {
         printf("You are at or below your average daily use (%.3lf GB/day).\n", Max_Use);
         double ExtraUse = (totalGB - total_used) / Remaning_Days;
         printf("You can use up to %.3lf GB/day and stay below your data plan limit\n", ExtraUse);
     }
-    
+
+    else
+    {
+        printf("You have already met your limit for this month. Looks like you're getting some overage charges...\n");
+    }
+
     return 0;
 }
